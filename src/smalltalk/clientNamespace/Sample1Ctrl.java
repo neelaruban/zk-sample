44f12ad21c08f4b2cf1e6bdf595a0e350e2b0cdf
whkuoattaiwan <whkuoattaiwan@c1d0113b-1b7d-d47c-39b5-b6d8e7e53f19>
2010-12-31 09:03:04 +0000

    add some sample

--- src/smalltalk/clientNamespace/Sample1Ctrl.java
+++ src/smalltalk/clientNamespace/Sample1Ctrl.java
@@ -3,10 +3,13 @@ package smalltalk.clientNamespace;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Html;
 import org.zkoss.zul.Label;
 
 public class Sample1Ctrl extends GenericForwardComposer{
 
+	private Html htmlComp;
+	
 	public void doAfterCompose(Component win) throws Exception {
 		Label label = (Label) win.getFellow("setByCtrl");
 		label.setWidgetListener("onClick", "this.setValue('onClick handler from java');");
@@ -15,4 +18,7 @@ public class Sample1Ctrl extends GenericForwardComposer{
 		label2.setWidgetOverride("setValue", "function (value) {this.$setValue('overloaded setValue');}");
 	}
 	
+	public void onUser$htmlComp(Event event){
+		System.out.println("onUser get in server");
+	}
 }
